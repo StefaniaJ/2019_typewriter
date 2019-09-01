2e2a96def4d5588420034766690b54cbdfd86261
Stefania <44357465+StefaniaJ@users.noreply.github.com>
2019-09-01 20:34:52 +0200

    typewritter done -without sounds

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,24 @@
 "use strict";
+
+let pos = 0;
+let speed = 180;
+let typewriter = document.querySelector("#typewriter").textContent;
+//console.log(typewriter);
+
+// empty the typewriter id from html
+document.querySelector("#typewriter").textContent = "";
+
+//function type
+function type() {
+  if (pos < typewriter.length) {
+    document.querySelector("#typewriter").textContent += typewriter.charAt(pos);
+    pos++;
+    setTimeout(type, speed); // call this again to type all letters
+  } else {
+    pos = 1;
+    document.querySelector("#typewriter").textContent = "";
+    setTimeout(type, speed);
+  }
+}
+
+setTimeout(type, speed);
